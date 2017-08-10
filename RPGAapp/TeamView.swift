7145a776db68da188ca2ee27c6515548fb55a9c9
Jakub <jakub@Jakubs-iMac.local>
2017-08-10 10:25:09 +0200

    Dodanie TeamView

---
+++ RPGAapp/TeamView.swift
@@ -0,0 +1,13 @@
+//
+//  TeamView.swift
+//  RPGAapp
+//
+//  Created by Jakub on 10.08.2017.
+//  Copyright © 2017 Jakub. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class TeamView: UIStackView{
+    
+}
