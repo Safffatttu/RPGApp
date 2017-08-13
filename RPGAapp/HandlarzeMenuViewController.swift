be85d91d80bf9e975ca11478806870d9c88b93e6
Jakub <jakub@Jakubs-iMac.local>
2017-08-13 12:47:20 +0200

    dodanie HandlarzeMenuViewController.swift i RandomItemMenuViewController.swift

---
+++ RPGAapp/HandlarzeMenuViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HandlarzeMenuViewController.swift
+//  RPGAapp
+//
+//  Created by Jakub on 12.08.2017.
+//  Copyright © 2017 Jakub. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class handlarzeMenu: UITableViewController{
+    
+}
---
+++ RPGAapp/RandomItemMenuViewController.swift
@@ -0,0 +1,14 @@
+//
+//  RandomItemMenuViewController.swift
+//  RPGAapp
+//
+//  Created by Jakub on 12.08.2017.
+//  Copyright © 2017 Jakub. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class randomItemMenu: UITableViewController {
+
+}
