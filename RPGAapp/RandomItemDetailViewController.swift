f499234ed5c49f8095d34927be3e5ea934932ab0
Jakub <jakub@Jakubs-iMac.local>
2017-08-24 21:44:20 +0200

    dodanie losowania itemów

--- RPGAapp/RandomItemDetailViewController.swift
+++ RPGAapp/RandomItemDetailViewController.swift
@@ -9,7 +9,134 @@
 import Foundation
 import UIKit
 
-class randomItemDetailView: UIViewController{
+class randomItemDetailView: UIViewController, UITableViewDataSource, UITableViewDelegate, randomItemCellDelegate{
+    
+    let iconSize: CGFloat = 20
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(reloadTableData), name: .reloadRandomItemTable, object: nil)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if randomlySelected.count > 0{
+            return randomlySelected.count
+        }
+        else{
+            return 1
+        }
+        
+    }
+    
+    func reloadTableData(_ notification: Notification) {
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "randomItemCell") as! randomItemCell
+        if randomlySelected.count > 0{
+            cell.nameLabel.text = listOfItems.items[randomlySelected[indexPath.row]].name
+            var priceToShow = String()
+            if  listOfItems.items[randomlySelected[indexPath.row]].price != nil  {
+                if settingValues["Show price"]!{
+                    priceToShow = changeCurrency(price: listOfItems.items[randomlySelected[indexPath.row]].price!, currency: listOfItems.currency)
+                }
+                else{
+                    priceToShow = String(listOfItems.items[randomlySelected[indexPath.row]].price!) + "PLN"
+                }
+            }
+            else {
+                priceToShow = "Brak ceny"
+                print(listOfItems.items[randomlySelected[indexPath.row]])
+            }
+            cell.priceLabel.text = priceToShow
+            
+        }
+        else{
+            cell.nameLabel?.text = "Jeszcze nie wylosowano przedmiotów"
+            cell.priceLabel?.text = ""
+            
+            cell.sendButton.isHidden = true
+            cell.infoButton.isHidden = true
+            cell.editButton.isHidden = true
+            cell.packageButton.isHidden = true
+        }
+        
+        
+        cell.sendButton.titleLabel?.font = UIFont.fontAwesome(ofSize: iconSize)
+        cell.sendButton.setTitle(String.fontAwesomeIcon(name: .send), for: .normal)
+        
+        cell.infoButton.titleLabel?.font = UIFont.fontAwesome(ofSize: iconSize)
+        cell.infoButton.setTitle(String.fontAwesomeIcon(name: .info), for: .normal)
+        
+        cell.editButton.titleLabel?.font = UIFont.fontAwesome(ofSize: iconSize)
+        cell.editButton.setTitle(String.fontAwesomeIcon(name: .edit), for: .normal)
+        
+        cell.packageButton.titleLabel?.font = UIFont.fontAwesome(ofSize: iconSize)
+        cell.packageButton.setTitle(String.fontAwesomeIcon(name: .cube), for: .normal)
+        return cell
+    }
+    
+    func addToPackageButton(_ sender: UIButton){
+    }
+    
+    func editItemButton(_ sender: UIButton){
+    }
+    
+    func showInfoButton(_ sender: UIButton){
+    }
+    
+    func sendItemButton(_ sender: UIButton){
+    }
+}
 
+class randomItemCell: UITableViewCell{
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    @IBOutlet var packageButton: UIButton!
+    
+    @IBOutlet var editButton: UIButton!
+    
+    @IBOutlet var infoButton: UIButton!
+    
+    @IBOutlet var sendButton: UIButton!
+    
+    weak var cellDelegate: randomItemCellDelegate?
+    
+    @IBAction func addToPackageButton(_ sender: UIButton) {
+        cellDelegate?.addToPackageButton(sender)
+    }
+    
+    @IBAction func editItemButton(_ sender: UIButton) {
+        cellDelegate?.editItemButton(sender)
+    }
+    
+    @IBAction func showInfoButton(_ sender: UIButton) {
+        cellDelegate?.showInfoButton(sender)
+    }
+    
+    @IBAction func sendItemButton(_ sender: UIButton) {
+        cellDelegate?.sendItemButton(sender)
+    }
+}
 
+protocol randomItemCellDelegate: class{
+    
+    func addToPackageButton(_ sender: UIButton)
+    
+    func editItemButton(_ sender: UIButton)
+    
+    func showInfoButton(_ sender: UIButton)
+    
+    func sendItemButton(_ sender: UIButton)
+    
 }
--- RPGAapp/RandomItemMenuViewController.swift
+++ RPGAapp/RandomItemMenuViewController.swift
@@ -9,6 +9,61 @@
 import Foundation
 import UIKit
 
+var randomlySelected = [Int]()
 class randomItemMenu: UITableViewController {
+    
+    let losowania = [("Broń", drawType.category,"BROŃ",2), ("Broń biała", drawType.subCategory,"BIAŁA",2)]
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return losowania.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "randomItemCell")
+        cell?.textLabel?.text = losowania[indexPath.row].0
+        return cell!
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        drawItems(type: losowania[indexPath.row].1, range: losowania[indexPath.row].2, numberOf: losowania[indexPath.row].3)
+        //print (listOfItems.items.filter({$0.category == losowania[indexPath.row].2}))
+    }
+    
+    func drawItems(type: drawType, range: String, numberOf: Int){
+        if !(settingValues["Dodawaj do listy wylosowanych"]!) {
+            randomlySelected = []
+        }
+        for _ in 1...numberOf{
+            var newItem: item
+            switch type{
+                case .category:
+                    newItem = drawItem(items: listOfItems.items.filter({$0.category == range}))
+                case .subCategory:
+                    newItem = drawItem(items: listOfItems.items.filter({$0.subCategory == range}))
+            }
+            randomlySelected.append(listOfItems.items.index{$0.name == newItem.name}!)
+        }
+        NotificationCenter.default.post(name: .reloadRandomItemTable, object: nil)
+        return
+    }
+    
+    func drawItem(items: [item]) -> item{
+        let weightedMap = items.map({($0,UInt($0.quantity!))})
+        print(weightedMap)
+        return weightedRandomElement(items: weightedMap)
+    }
+    
+}
+extension Notification.Name{
+    static let reloadRandomItemTable = Notification.Name("reloadRandomItemTable")
+}
 
+
+enum drawType: Int{
+    case category
+    case subCategory
 }
