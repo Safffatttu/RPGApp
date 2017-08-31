cd7eb5a9f35785dc6d884916d51fc08c7c8e35a9
Jakub <jakub@Jakubs-iMac.local>
2017-09-01 01:35:17 +0200

    Przystosowanie do CoreData

--- RPGAapp/sendAllPopoverViewController.swift
+++ RPGAapp/sendAllPopoverViewController.swift
@@ -9,17 +9,28 @@
 import Foundation
 import UIKit
 import FontAwesome_swift
+import CoreData
 
 class sendAllPopover: UITableViewController, sendAllPopoverDelegate{
     
-    var items: [item]? = nil
+    override func viewWillAppear(_ animated: Bool) {
+        let context = CoreDataStack.managedObjectContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Character")
+        
+        do {
+            newTeam = try context.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if (team.count > 0){
-            return team.count
+        if (newTeam.count > 0){
+            return newTeam.count
         }
         else{
             return 1
@@ -29,8 +40,8 @@ class sendAllPopover: UITableViewController, sendAllPopoverDelegate{
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "sendAllPopoverCell") as! sendAllPopoverCell
         cell.cellDelegate = self
-        if (team.count > 0){
-            cell.playerName.text = team[indexPath.row].name
+        if (newTeam.count > 0){
+            cell.playerName.text = (newTeam[indexPath.row] as! Character).name
             cell.sendButton.titleLabel?.font = UIFont.fontAwesome(ofSize: 20)
             cell.sendButton.setTitle(String.fontAwesomeIcon(name: .send), for: .normal)
         }
@@ -51,9 +62,9 @@ class sendAllPopover: UITableViewController, sendAllPopoverDelegate{
     
     func sendItem(_ sender: UIButton) {
         let playerNum = getCurrentCellIndexPath(sender)?.row
-        for item in randomlySelected{
-            team[playerNum!].items?.append(item)
-        }
+        let senTo = newTeam[playerNum!] as! Character
+        senTo.addToEquipment(NSOrderedSet(array: randomlySelected))
+        CoreDataStack.saveContext()
         dismiss(animated: true, completion: nil)
     }
     
