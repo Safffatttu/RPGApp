457b5e3ea8af6dd77add01b7149fe6c9f7375694
Jakub <jakub@Jakubs-iMac.local>
2017-09-02 12:17:53 +0200

    Ładowanie przedmiotów z kategoriami jako relacjami

--- RPGAapp/AppDelegate.swift
+++ RPGAapp/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import CoreData
+import Foundation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
@@ -20,7 +22,59 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
         navigationController.topViewController?.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
         splitViewController.delegate = self
+        let defaults = UserDefaults.standard
+        let isPreloaded = defaults.bool(forKey: "isPreloaded")
+        if !isPreloaded {
+            DispatchQueue.global(qos: .userInitiated).async {
+                self.preloadData()
+                defaults.set(true, forKey: "isPreloaded")
+            }
+        }
+        reloadCoreData()
         return true
+    }
+    
+    
+    func preloadData () {
+        let context = CoreDataStack.managedObjectContext
+        var currentCategory: String = ""
+        var currentSubCategory: String = ""
+        var newCategory: Category? = nil
+        var newSubCategory: SubCategory? = nil
+        for item in listOfItems.items {
+            if item.category != currentCategory{
+                currentCategory = item.category
+                newCategory = NSEntityDescription.insertNewObject(forEntityName: "Category", into: context) as! Category
+                newCategory!.setValue(currentCategory, forKey: #keyPath(Category.name))
+
+            }
+            if item.subCategory != currentSubCategory{
+                currentSubCategory = item.subCategory
+                newSubCategory = NSEntityDescription.insertNewObject(forEntityName: "SubCategory", into: context) as! SubCategory
+                newSubCategory!.name = currentSubCategory
+            }
+            
+            let itemToSave = NSEntityDescription.insertNewObject(forEntityName: "Item", into: context)
+            itemToSave.setValue(item.description, forKey: "item_description")
+            itemToSave.setValue(item.measure, forKey: "measure")
+            itemToSave.setValue(item.name, forKey: "name")
+            itemToSave.setValue(item.price, forKey: "price")
+            itemToSave.setValue(item.quantity, forKey: "quantity")
+            itemToSave.setValue(item.rarity, forKey: "rarity")
+            
+            newCategory!.addToItems(itemToSave as! Item)
+            newSubCategory!.addToItems(itemToSave as! Item)
+        }
+        CoreDataStack.saveContext()
+        return
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        CoreDataStack.saveContext()
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        CoreDataStack.saveContext()
     }
     
     
@@ -78,6 +132,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         }
         return nil
     }
+    /*
+    // MARK: - CORE DATA
+    lazy var applicationDocumentsDirectory: URL = {
+        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.cadiridris.coreDataTemplate" in the application's documents Application Support directory.
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return urls[urls.count-1]
+    }()
+    
+    lazy var managedObjectModel: NSManagedObjectModel = {
+        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
+        let modelURL = Bundle.main.url(forResource: "coreDataTemplate", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelURL)!
+    }()
+    
+    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
+        // Create the coordinator and store
+        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.appendingPathComponent("SingleViewCoreData.sqlite")
+        var failureReason = "There was an error creating or loading the application's saved data."
+        do {
+            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: nil)
+        } catch {
+            // Report any error we got.
+            var dict = [String: AnyObject]()
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data" as AnyObject?
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason as AnyObject?
+            
+            dict[NSUnderlyingErrorKey] = error as NSError
+            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
+            // Replace this with code to handle the error appropriately.
+            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+            abort()
+        }
+        
+        return coordinator
+    }()*/
     
 }
 
