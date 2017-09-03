a8a23fb57a5c9427cda71c4247bf39d0f342f0c7
Jakub <jakub@Jakubs-iMac.local>
2017-09-03 21:42:15 +0200

    Implementacja zawansowanych losowań opartych o drawSetting z CoreData

--- RPGAapp/RandomItemMenuViewController.swift
+++ RPGAapp/RandomItemMenuViewController.swift
@@ -16,71 +16,72 @@ class randomItemMenu: UITableViewController {
     
     fileprivate let drawQueue = DispatchQueue(label: "com.SS.RPGAapp")
     
+    var drawSettings: [DrawSetting] = []
     
-    let losowania = [("Broń", drawType.category,"BROŃ",100000), ("Broń biała", drawType.subCategory,"BIAŁA",2)]
+    override func viewDidLoad() {
+        let context = CoreDataStack.managedObjectContext
+        let drawSettingsFetch: NSFetchRequest<DrawSetting> = DrawSetting.fetchRequest()
+        do{
+            drawSettings = try context.fetch(drawSettingsFetch) as [DrawSetting]
+        }
+        catch{
+            print("error")
+        }
+        print(drawSettings.map{$0.name})
+        
+    }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return  1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return losowania.count
+        return drawSettings.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "randomItemCell")
-        cell?.textLabel?.text = losowania[indexPath.row].0
+        cell?.textLabel?.text = drawSettings[indexPath.row].name
         return cell!
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         drawQueue.async {
-            self.drawItems(type: self.losowania[indexPath.row].1, range: self.losowania[indexPath.row].2, numberOf: self.losowania[indexPath.row].3)
+            self.drawItems(drawSetting: self.drawSettings.first!)
             DispatchQueue.main.async {
                 NotificationCenter.default.post(name: .reloadRandomItemTable, object: nil)
             }
         }
     }
     
-    func drawItems(type: drawType, range: String, numberOf: Int){
-        if !(UserDefaults.standard.bool(forKey: "Dodawaj do listy wylosowanych")) {
-            randomlySelected = []
-        }
-
-        var itemsToDraw: [Item] = []
-        let context = CoreDataStack.managedObjectContext
+    func drawItems(drawSetting: DrawSetting){
+        let subSettings: [DrawSubSetting] = drawSetting.subSettings?.sortedArray(using: [NSSortDescriptor(key: #keyPath(DrawSubSetting.name), ascending: true)]) as! [DrawSubSetting]
         
-        switch type{
-            case .category:
-                let fetchRequest = NSFetchRequest<Category>(entityName: NSStringFromClass(Category))
-                fetchRequest.predicate = NSPredicate(format: "name == %@", range)
-                do{
-                    itemsToDraw = try (context.fetch(fetchRequest).first!).items?.sortedArray(using: [sortItemByName]) as! [Item]
-                }
-                catch{
-                    print("Error")
-                }
+        for setting in subSettings{
+            var itemsToDraw: [Item]
+            let weight: Int
             
-            case .subCategory:
-                let fetchRequest = NSFetchRequest<SubCategory>(entityName: NSStringFromClass(SubCategory.self))
-                fetchRequest.predicate = NSPredicate(format: "name == %@", range)
-                do{
-                    itemsToDraw = try (context.fetch(fetchRequest).first!).items?.sortedArray(using: [sortItemByName]) as! [Item]
-                }
-                catch{
-                    print("Error")
-                }
-        }
-
-        itemsToDraw = itemsToDraw.map{
-            $0.propability = propabilities[Int(Int(($0).rarity) - 1)]
-            return $0
-        }
-        
-        let weightTotal = Int (itemsToDraw.map{$0.propability}.reduce(0, +))
-        for _ in 1...numberOf{
-            let newItem = drawItem(items: itemsToDraw, weightTotal: weightTotal)
-            randomlySelected.append(newItem)
+            let numberOf = Int(setting.itemsToDraw)
+            
+            if(setting.category != nil){
+                itemsToDraw = setting.category?.items?.sortedArray(using: [NSSortDescriptor(key: #keyPath(Item.name), ascending: true)]) as! [Item]
+            }
+            else{
+                itemsToDraw = setting.subCategory?.items?.sortedArray(using: [NSSortDescriptor(key: #keyPath(Item.name), ascending: true)]) as! [Item]
+            }
+            
+            itemsToDraw = itemsToDraw.map{
+                $0.propability = propabilities[Int(Int(($0).rarity) - 1)]
+                return $0
+            }
+            
+            weight = Int (itemsToDraw.map{$0.propability}.reduce(0, +))
+            print(numberOf)
+            
+            for _ in 1...numberOf{
+                let newItem = drawItem(items: itemsToDraw, weightTotal: weight)
+                randomlySelected.append(newItem)
+            }
         }
         return
     }
