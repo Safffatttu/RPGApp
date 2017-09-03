18779e9e19d4fc633152931db20cf76bdaa6ff6c
Jakub <jakub@Jakubs-iMac.local>
2017-09-03 18:27:35 +0200

    cena itemu jest opcjonalna

--- RPGAapp/Data Classes/Item+CoreDataProperties.swift
+++ RPGAapp/Data Classes/Item+CoreDataProperties.swift
@@ -19,7 +19,7 @@ extension Item {
     @NSManaged public var item_description: String?
     @NSManaged public var measure: String?
     @NSManaged public var name: String?
-    @NSManaged public var price: Double
+    @NSManaged public var price: Double?
     @NSManaged public var quantity: Int16
     @NSManaged public var rarity: Int16
     @NSManaged public var propability: Int16
