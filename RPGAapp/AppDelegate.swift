c2e5d0c338d5b4145f9927949447bdeef657997e
Jakub <jakub@Jakubs-iMac.local>
2017-09-24 15:03:38 +0200

    Usunięcie zbędnego kodu

--- RPGAapp/AppDelegate.swift
+++ RPGAapp/AppDelegate.swift
@@ -25,76 +25,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         let defaults = UserDefaults.standard
         let isPreloaded = defaults.bool(forKey: "isPreloaded")
         if !isPreloaded {
-            DispatchQueue.global(qos: .userInitiated).async {
+            DispatchQueue.global(qos: .utility).async {
                 loadItemsFromAsset()
                 defaults.set(true, forKey: "isPreloaded")
-                print("aa")
             }
         }
         reloadCoreData()
         return true
-    }
-    
-    
-    func preloadData () {
-        let context = CoreDataStack.managedObjectContext
-        var currentCategory: String = ""
-        var currentSubCategory: String = ""
-        var newCategory: Category? = nil
-        var newSubCategory: SubCategory? = nil
-        for item in listOfItems.items {
-            if item.category != currentCategory{
-                currentCategory = item.category
-                newCategory = NSEntityDescription.insertNewObject(forEntityName: "Category", into: context) as! Category
-                newCategory!.setValue(currentCategory, forKey: #keyPath(Category.name))
-
-            }
-            if item.subCategory != currentSubCategory{
-                currentSubCategory = item.subCategory
-                newSubCategory = NSEntityDescription.insertNewObject(forEntityName: "SubCategory", into: context) as! SubCategory
-                newSubCategory!.setValue(currentSubCategory, forKey: #keyPath(SubCategory.name))
-                newCategory!.addToSubCategories(newSubCategory!)
-            }
-            
-            let itemToSave = NSEntityDescription.insertNewObject(forEntityName: "Item", into: context)
-            itemToSave.setValue(item.description, forKey: #keyPath(Item.item_description))
-            itemToSave.setValue(item.measure, forKey: #keyPath(Item.measure))
-            itemToSave.setValue(item.name, forKey: #keyPath(Item.name))
-            itemToSave.setValue(item.price, forKey: #keyPath(Item.price))
-            itemToSave.setValue(item.quantity, forKey: #keyPath(Item.quantity))
-            itemToSave.setValue(item.rarity, forKey: #keyPath(Item.rarity))
-            
-            newCategory!.addToItems(itemToSave as! Item)
-            newSubCategory!.addToItems(itemToSave as! Item)
-        }
-        
-        var newSetting: DrawSetting
-        var newSubSetting: DrawSubSetting
-        
-        newSubSetting = NSEntityDescription.insertNewObject(forEntityName: String(describing: DrawSubSetting.self), into: context) as! DrawSubSetting
-        newSubSetting.itemsToDraw = 10
-        newSubSetting.category = newCategory
-        
-        newSetting = NSEntityDescription.insertNewObject(forEntityName: String(describing: DrawSetting.self), into: context) as! DrawSetting
-        newSetting.setValue("Broń", forKey: #keyPath(DrawSetting.name))
-        newSetting.addToSubSettings(newSubSetting)
-        
-        /*------------------------------------------------------*/
-        
-        var newSetting2: DrawSetting
-        var newSubSetting2: DrawSubSetting
-      
-        newSubSetting2 = NSEntityDescription.insertNewObject(forEntityName: String(describing: DrawSubSetting.self), into: context) as! DrawSubSetting
-        newSubSetting2.subCategory = newSubCategory
-        newSubSetting2.itemsToDraw = 4
-        
-        newSetting2 = NSEntityDescription.insertNewObject(forEntityName: String(describing: DrawSetting.self), into: context) as! DrawSetting
-        newSetting2.setValue("Inna broń", forKey: #keyPath(DrawSetting.name))
-        newSetting2.addToSubSettings(newSubSetting2)
-        
-        
-        CoreDataStack.saveContext()
-        return
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
@@ -129,11 +66,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         }
                
         return false
-        
-        
-        
-        
-        
     }
     
     // Correctly Handle Portrait to Landscape transition for iPhone 6+ when TableView2 is open in Portrait. Comment and see for yourself, what happens when you don't write this.
@@ -148,63 +80,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
                 return (UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "detail1Nav") )
             
             }
-            
-            
         }
         return nil
-    }
-    /*
-    // MARK: - CORE DATA
-    lazy var applicationDocumentsDirectory: URL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "com.cadiridris.coreDataTemplate" in the application's documents Application Support directory.
-        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return urls[urls.count-1]
-    }()
-    
-    lazy var managedObjectModel: NSManagedObjectModel = {
-        // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = Bundle.main.url(forResource: "coreDataTemplate", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOf: modelURL)!
-    }()
-    
-    lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
-        // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        // Create the coordinator and store
-        let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-        let url = self.applicationDocumentsDirectory.appendingPathComponent("SingleViewCoreData.sqlite")
-        var failureReason = "There was an error creating or loading the application's saved data."
-        do {
-            try coordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: url, options: nil)
-        } catch {
-            // Report any error we got.
-            var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data" as AnyObject?
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason as AnyObject?
-            
-            dict[NSUnderlyingErrorKey] = error as NSError
-            let wrappedError = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            // Replace this with code to handle the error appropriately.
-            // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-            NSLog("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
-            abort()
-        }
-        
-        return coordinator
-    }()*/
-    
 }
-
-
-/* domyślny kod
-// MARK: - Split view
-
-func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
-    guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-    guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
-    if topAsDetailController.detailItem == nil {
-        // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-        return true
-    }
-    return false
-}
-*/
