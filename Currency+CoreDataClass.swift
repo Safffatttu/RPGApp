06b9e3a2ddb4e27dbcbd7d25f175de0e1df28d3d
Jakub <jakub@Jakubs-iMac.local>
2017-09-29 14:43:42 +0200

    Klasa Currency w dataModel

---
+++ Currency+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Currency+CoreDataClass.swift
+//  
+//
+//  Created by Jakub on 21.09.2017.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Currency)
+public class Currency: NSManagedObject {
+
+}
