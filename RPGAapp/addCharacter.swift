a14498eefd2b78c2cb4a715e7640990ef7375e0b
Jakub <jakub@jakubs-imac.local>
2017-11-14 21:48:00 +0100

    Naprawa bugu - xcode nie mógł odnaleźć klasy addCharacter dla popovera

--- RPGAapp/addCharacter.swift
+++ RPGAapp/addCharacter.swift
@@ -1,8 +1,8 @@
-///Users/jakub/Dysk Google/Xcode/RPGAapp/RPGAapp
-//  addCharacterModalViewController.swift
+//
+//  addCharacter.swift
 //  RPGAapp
 //
-//  Created by Jakub on 19.08.2017.
+//  Created by Jakub on 13.11.2017.
 //  Copyright © 2017 Jakub. All rights reserved.
 //
 
@@ -10,7 +10,7 @@ import Foundation
 import UIKit
 import CoreData
 
-class addCharacterView: UIViewController {
+class addCharacter: UIViewController {
     
     @IBOutlet weak var nameField: UITextField!
     
@@ -22,15 +22,10 @@ class addCharacterView: UIViewController {
     
     @IBOutlet weak var hpField: UILabel!
     
-    @IBAction func stepperChanged(_ sender: UIStepper) {
-        hpField.text = String(describing: Int(sender.value)) + "HP"
-    }
-
-    override func viewWillAppear() {
+    override func viewWillAppear(_ animated: Bool) {
         self.preferredContentSize = CGSize(width: 300, height: 400)
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(addCharacter(_:)))
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(dismissView(_:)))
-        //hpField.text = String(describing: Int(hpStepper.value)) + "HP"
     }
     
     func dismissView(_ sender: UIBarButtonItem) {
@@ -45,7 +40,8 @@ class addCharacterView: UIViewController {
         let newCharacter = NSEntityDescription.insertNewObject(forEntityName: String(describing: Character.self), into: CoreDataStack.managedObjectContext)
         newCharacter.setValue(nameField.text!, forKey: #keyPath(Character.name))
         newCharacter.setValue(hpStepper.value, forKey: #keyPath(Character.health))
-        newCharacter.setValue(nameField.text! + nameField.text.hash, forKey: #keyPath(Character.id))
+        let id = nameField.text! + String((nameField.text! + UIDevice.current.name).hash)
+        newCharacter.setValue(nameField.text! + id, forKey: #keyPath(Character.id))
         CoreDataStack.saveContext()
         
         NotificationCenter.default.post(name: .reloadTeam, object: nil)
