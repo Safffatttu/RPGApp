b6dbbd87a713c8798ac771e1ae0211f5aa714bfe
Jakub <jakub@jakubs-imac.local>
2017-11-16 19:48:25 +0100

    Usuwanie przedmiotów od postaci

--- RPGAapp/TeamView.swift
+++ RPGAapp/TeamView.swift
@@ -86,6 +86,25 @@ extension TeamView: UITableViewDataSource, UITableViewDelegate{
         }
     }
     
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        if tableView.dequeueReusableCell(withIdentifier: "itemCell") != nil{
+            return true
+        }else{
+            return false
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell")
+        if cell != nil && editingStyle == .delete{
+            let equipment = (newTeam[tableView.tag] as! Character).equipment!.sortedArray(using: [sortItemHandlerByName]) as! [ItemHandler]
+            (newTeam[tableView.tag] as! Character).removeFromEquipment(equipment[indexPath.row])
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            CoreDataStack.saveContext()
+            return
+        }
+    }
+    
     
 }
 
@@ -98,7 +117,6 @@ class TeamViewCell: UICollectionViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     
     func setTableViewDataSourceDelegate<D: UITableViewDataSource & UITableViewDelegate>(_ dataSourceDelegate: D, forRow row: Int) {
-        //print(dataSourceDelegate.description)
         table.delegate = dataSourceDelegate
         table.dataSource = dataSourceDelegate
         table.tag = row
