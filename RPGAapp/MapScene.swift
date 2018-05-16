910235c6c5e8d8ee418a47b6c58d6522ffda6121
myname <me@me.com>
2018-05-16 19:57:14 +0200

    Implementation of SpriteKit to MapViewControler

---
+++ RPGAapp/MapScene.swift
@@ -0,0 +1,53 @@
+//
+//  MapScene.swift
+//  RPGAapp
+//
+//  Created by Jakub on 16.05.2018.
+//  Copyright © 2018 Jakub. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class MapScene: SKScene{
+	
+	override func didMove(to view: SKView) {
+		
+	}
+	
+	
+	func touchDown(atPoint pos : CGPoint) {
+		
+	}
+	
+	func touchMoved(toPoint pos : CGPoint) {
+		
+	}
+	
+	func touchUp(atPoint pos : CGPoint) {
+		
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+	}
+	
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+	}
+	
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+	}
+	
+	override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+		for t in touches { self.touchUp(atPoint: t.location(in: self)) }
+	}
+	
+	
+	override func update(_ currentTime: TimeInterval) {
+		// Called before each frame is rendered
+	}
+	
+	
+}
