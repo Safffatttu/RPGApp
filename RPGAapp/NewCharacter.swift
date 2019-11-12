3b33b61f1f9da85d11e726ee4ae34b3c30f1c20f
myname <me@me.com>
2019-11-12 19:57:08 +0100

    Refactored parts generated by automatic swift migrator

--- RPGAapp/NewCharacter.swift
+++ RPGAapp/NewCharacter.swift
@@ -224,24 +224,10 @@ extension NewCharacterForm: UIImagePickerControllerDelegate, UINavigationControl
 	}
 
 	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
-// Local variable inserted by Swift 4.2 migrator.
-let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
-
-		textureImage = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.editedImage)] as? UIImage
+        textureImage = info[.editedImage] as? UIImage
 	
 		dismiss(animated: true, completion: nil)
 	
 		self.imageRow.update()
-	
-	}
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
-	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
-}
-
-// Helper function inserted by Swift 4.2 migrator.
-fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
-	return input.rawValue
+    }
 }
